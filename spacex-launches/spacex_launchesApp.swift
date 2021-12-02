c2f2dcd445f38c99065c50dfaf57926ca48fe224
Pavel Martynenkov <pashok.resh@yandex.ru>
2021-12-02 22:49:42 +0300

    project created

---
+++ spacex-launches/spacex_launchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  spacex_launchesApp.swift
+//  spacex-launches
+//
+//  Created by Pavel on 02.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct spacex_launchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
